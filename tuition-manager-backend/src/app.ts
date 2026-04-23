122308f890736098da9406dd41be0706a7d8bacd
vidyarthinikhil09 <vidyarthinikhil2@gmail.com>
2026-04-23 08:32:42 +0530

    Added trust Proxy

--- tuition-manager-backend/src/app.ts
+++ tuition-manager-backend/src/app.ts
@@ -13,6 +13,8 @@ import { errorHandler } from './middlewares/errorHandler';
 
 const app = express();
 
+app.set('trust proxy', 1);
+
 app.use(cors({
   origin: process.env.CLIENT_URL || 'http://localhost:3000', // Vite frontend URL
   credentials: true
